0a9fbacdb7893986ffc0d096f759d7215c4eba86
Isa-Paiola <isabelapaiola6@gmail.com>
2025-09-30 10:11:31 -0300

    commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+// Configuração da conexão com o banco de dados MySQL para a Esmalteria
+
+$servername = "localhost"; // Nome do servidor do banco (localmente localhost)
+$username = "root";        // Usuário do banco de dados
+$password = "";            // Senha do banco (vazia no XAMPP padrão)
+$dbname = "esmalteria_db";   // Nome do banco de dados da pizzaria
+
+// Cria a conexão com o banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica se houve erro na conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+// Definição do charset para evitar problemas com acentuação
+$conn->set_charset("utf8");
+?>
